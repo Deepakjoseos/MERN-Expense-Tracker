d9a8cf58970db26f54d82f4c516666734eaf2bd6
Deepakjoseos <josedipu9@gmail.com>
2024-03-19 13:45:04 +0530

    Add files

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
 const { db } = require('./db/db');
 const {readdirSync} = require('fs')
@@ -12,6 +12,9 @@ const PORT = process.env.PORT || 5000
 app.use(express.json())
 app.use(cors())
 
+app.get('/',(req,res,next)=>{
+    res.send("<h1>Welcome to MERN Expense Tracker Server</h1>");
+  })
 //routes
 readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
 
